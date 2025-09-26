2f4146bf4c27420acf0dd4ac5234cef27c86545e
paranoidromulan <veronylie@gmail.com>
2025-09-26 09:21:17 +0300

    Basic DB usage

--- Kauppakanta.cs
+++ Kauppakanta.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Data.Sqlite;
 
 class Kauppakanta
@@ -21,6 +22,43 @@ class Kauppakanta
             tablecmd.CommandText = @"Create TABLE IF NOT EXISTS
             Asiakaat (id INTEGER PRIMARY KEY, nimi TEXT)";
             tablecmd.ExecuteNonQuery();
+        }
+
+    }
+    //Metodi, jolla listaöön asiakas tietokantan
+
+    public void LisaaAsiakas(string nimi)
+    {
+        using (var connection = new SqliteConnection(_connectionstring))
+        {
+            connection.Open();
+            //Lisäätän asiakas tietokantan
+
+            var insertcmd = connection.CreateCommand();
+            insertcmd.CommandText = @"INSERT INTO Asiakaat (nimi) VALUES ($nimi)";
+            insertcmd.Parameters.AddWithValue($"nimi", nimi);
+            insertcmd.ExecuteNonQuery();
+        }
+    }
+    public Dictionary<int, string> HaeAsiakaat()
+    {
+        using (var connection = new SqliteConnection(_connectionstring))
+        {
+            connection.Open();
+
+            //Haetaan kaikki asiakaat tietokannasta
+
+            var selectcmd = connection.CreateCommand();
+            selectcmd.CommandText = "SELECT * FROM Asiakaat";
+            using (var reader = selectcmd.ExecuteReader())
+            {
+                var asiakaat = new Dictionary<int, string>();
+                while (reader.Read())
+                {
+                    asiakaat.Add(reader.GetInt32(0), reader.GetString(1));
+                }
+                return asiakaat;
+            }
         }
     }
 }
